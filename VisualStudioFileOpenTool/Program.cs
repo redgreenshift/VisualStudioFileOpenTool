e27f3b3104e3ab177eff4b5dee504d794c4b6f57
DESKTOP-F1I7BHR\jared <jared.ivey@outlook.com>
2026-07-10 22:32:58 -0700

    minor loop prefix iterator change

--- VisualStudioFileOpenTool/Program.cs
+++ VisualStudioFileOpenTool/Program.cs
@@ -62,7 +62,7 @@ namespace VisualStudioFileOpenTool
 		{
 			if (value == null)
 				return true;
-			for (int i = 0; i < value.Length; i++)
+			for (int i = 0; i < value.Length; ++i)
 			{
 				if (!char.IsWhiteSpace(value[i]))
 					return false;
