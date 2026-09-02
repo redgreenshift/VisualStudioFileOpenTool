d89ea3445192b551e2640e5ae0e4bf91dd527fae
DESKTOP-F1I7BHR\jared <jared.ivey@outlook.com>
2026-09-01 17:28:39 -0700

    update target Framework version to v3.5

--- VisualStudioFileOpenTool/Properties/AssemblyInfo.cs
+++ VisualStudioFileOpenTool/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VSSameInstaneOpenFileAtLine")]
+[assembly: AssemblyTitle("VisualStudioFileOpenTool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VSSameInstaneOpenFileAtLine")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("VisualStudioFileOpenTool")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
